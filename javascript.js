b6b952fbac248bd4bf0f03f3c18a1d6993af6650
Doubzero <131899921+Doubzero@users.noreply.github.com>
2023-05-23 10:37:54 -0400

    Added playround function.

--- javascript.js
+++ javascript.js
@@ -4,3 +4,22 @@ function getComputerChoice() {
   let item = compChoice[randomComputerChoice];
   return item;
 }
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection == "paper" && playerSelection == "rock") {
+    compScore++;
+    return "You Lose!, Paper Beats Rock";
+  } else if (computerSelection == "rock" && playerSelection == "scissor") {
+    compScore++;
+    return "You Lose!, Rock beats Scissor";
+  } else if (computerSelection == "scissor" && playerSelection == "paper") {
+    compScore++;
+    return "You Lose!, Scissor Beats Paper";
+  } else if (computerSelection === playerSelection) {
+    return "Draw";
+  } else {
+    console.log(
+      `You win! You chose ${playerSelection} and the computer chose ${computerSelection}`
+    );
+    playerScore++;
+  }
+}
